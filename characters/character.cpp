39d9436a317e710f19270b5fe2ea5a546c2d0682
Denis Vergun <blu.eagle@mail.ru>
2023-05-20 21:36:20 +0500

    cd characters;make

--- characters/character.cpp
+++ characters/character.cpp
@@ -73,6 +73,24 @@ class Character {
 	void display();
 
 };
+class BarbarianCharacter : public Character{
+
+	public: BarbarianCharacter() {
+                //this->charismaCharacter = new CharismaAwkward();
+                //this->constitutionCharacter = new ConstitutionHealthy();
+                //this->dextrityCharacter = new DexteritySneaky();
+                //this->intelligenceCharacter = new IntelligenceInquisitive();
+                //this->strengthCharacter = new StrengthBurly();
+                //this->wisdomCharacter = new WisdomFoolish();
+        }
+        
+        //@Override
+	public: void display() {
+                cout<<"Hi, I am Barbarian Character in this D&D popular game."<<endl;
+        }
+
+};
+
 class RangerCharacter : public Character {
 
         public: RangerCharacter() {
@@ -101,6 +119,21 @@ int main(){
                 ranger->performIntelligenceCharacter();
                 ranger->performStrengthCharacter();
                 ranger->performWisdomCharacter();
+	
+		BarbarianCharacter* barbarian = new BarbarianCharacter();
+                barbarian->display();
+                barbarian->setCharismaCharacter(new CharismaConfidence());
+                barbarian->performCharismaCharacter();
+                barbarian->setConstitutionCharacter(new ConstitutionStrong());
+                barbarian->performConstitutionCharacter();
+                barbarian->setDextrityCharacter(new DexteritySneaky());
+                barbarian->performDextrityCharacter();
+                barbarian->setIntelligenceCharacter(new IntelligenceForgetful());
+                barbarian->performIntelligenceCharacter();
+                barbarian->setStrengthCharacter(new StrengthScrawny());
+                barbarian->performStrengthCharacter();
+                barbarian->setWisdomCharacter(new WisdomOblivious());
+                barbarian->performWisdomCharacter();
 
 
 
