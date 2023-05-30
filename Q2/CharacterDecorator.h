e084a177a49b23cd80d96cd83f74d4465b80c1f3
Denis Vergun <blu.eagle@mail.ru>
2023-05-30 19:04:18 +0500

    work dec

--- Q2/CharacterDecorator.h
+++ Q2/CharacterDecorator.h
@@ -8,12 +8,12 @@ class CharacterDecorator : public IRanger{
 	public: T* rangerCharacterDecorator;
 
 	public: 
-	CharacterDecorator(IRanger* rangerCharacterDecorator)	
+	CharacterDecorator(IRanger* rangerCharacterDecorator):rangerCharacterDecorator((T*)rangerCharacterDecorator)	
 	{
         //cout<<static_cast<T*>(this)->getClothings();
 
 	//IRanger::IRanger(rangerCharacterDecorator);
-	this->rangerCharacterDecorator = (T*)rangerCharacterDecorator;
+	//this->rangerCharacterDecorator = (T*)rangerCharacterDecorator;
 //cout<<rangerCharacterDecorator->getCharisma()<<endl;
 	}
 	CharacterDecorator(){};
