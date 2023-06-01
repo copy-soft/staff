366121d71e4c00000951c603de860cf9c2e95336
Denis Vergun <blu.eagle@mail.ru>
2023-06-01 20:35:45 +0500

    make

--- mweap/item.h
+++ mweap/item.h
@@ -1,6 +1,7 @@
 #include <string>
 class Item{
 	public:
+	Item()=default;
 	virtual ~Item()=default;
 	virtual std::string GetName()const = 0 ;
 	private:
@@ -12,22 +13,133 @@ class Item{
 class Weapon:public Item{
 	public:
 	Weapon(std::string n, int d):Item(n),damage(d){}
-	std::string GetName()const override {return name;}
-	int GetDamage() const {return damage;}
+	virtual std::string GetName()const override {return name;}
+	virtual int GetDamage() const {return damage;}
 	protected:
 	int damage;
 
 
 };
+
 class MagicWeapon : public Weapon{
 	public:
 	MagicWeapon(std::string n, int d, int md): Weapon(n, d), magic_damage(md){};
-	int GetDamage() const {return damage+magic_damage;};
+	virtual int GetDamage() const {return damage+magic_damage;};
 	protected:
 	int magic_damage;
 
 };
 
 
+class ConcreteWeapon : public Item{
+        public:
+        ConcreteWeapon(std::string n, int d): Item(n), damage(d){};
+        virtual std::string GetName()const override {return name;}
+        virtual int GetDamage() const {return damage;};
+        protected:
+        int damage;
 
+};
+
+
+
+class ConcreteArmor : public Item{
+        public:
+        ConcreteArmor(std::string n, int dr): Item(n), damage_res(dr){};
+        virtual std::string GetName()const override {return name;}
+        virtual int GetDamageRes() const {return damage_res;};
+        protected:
+        int damage_res;
+
+};
+
+enum ITEMTYPE {UNKNOWN, ARMOR, WEAPON};
+ITEMTYPE WhatIsItemType(Item* item){
+
+auto* the_cast_to_armor = dynamic_cast<ConcreteArmor*>(item);
+if (the_cast_to_armor)
+	return ITEMTYPE::ARMOR;
+auto* the_cast_to_weapon = dynamic_cast<ConcreteWeapon*>(item);
+if (the_cast_to_weapon)
+	return ITEMTYPE::WEAPON;
+
+	return ITEMTYPE::UNKNOWN;
+}
+std::string WhatIsItemTypeString(Item* item){
+
+auto* the_cast_to_armor = dynamic_cast<ConcreteArmor*>(item);
+if (the_cast_to_armor)
+        return "ARMOR";
+auto* the_cast_to_weapon = dynamic_cast<ConcreteWeapon*>(item);
+if (the_cast_to_armor)
+	return "WEAPON";
+
+        return "UNKNOWN";
+}
+
+
+
+
+
+
+
+
+/*
+std::string WhatIsItemTypeString(Item* item){
+auto* this_item_type;
+        switch(this_item_type){
+                case ITEMTYPE::ARMOR:
+                return "Armor";
+                break;
+                case ITEMTYPE::WEAPON:
+                return "Weapon";
+                break;
+                case ITEMTYPE::UNKNOWN:
+                break "Unknown";
+                case default:
+                return "Unhandled";
+                break;
+        }
+        };
+*/
+
+
+class Magic : public Item{
+        public:
+        Magic(Item* item, int mod): item_(item), magic_modifier_(mod){};
+        //int GetDamage() const {return damage+magic_damage;};
+        protected:
+	Item* item_;
+        int magic_modifier_;
+
+};
+
+class MagicDecorator : public Item{
+        public:
+        MagicDecorator(Item* item, int mod): item_(item), magic_modifier_(mod){
+	}
+	std::string GetName() const{return WhatIsItemTypeString(item_);}
+
+	//WhatIsItemTypeString(item_);
+	/*switch(this_item_type){
+		case ITEMTYPE::ARMOR:
+		return "Armor";
+		break;
+		case ITEMTYPE::WEAPON:
+		return "Weapon";
+		break;
+		case ITEMTYPE::UNKNOWN:
+		break "Unknown";
+		case default:
+		return "Unhandled";
+		break;
+	}*/
+	//};
+        //int GetDamage() const {return damage+magic_damage;};
+        int GetMagicMod(){return magic_modifier_;}
+	protected:
+        Item* item_;
+        int magic_modifier_;
+
+};
 
--- mweap/main.cpp
+++ mweap/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "item.h"
 int main(){
-Weapon weapon1("Basic Sword", 3);
-std::cout<<weapon1.GetName()<< " damage: "<< weapon1.GetDamage()<<std::endl;
+ConcreteWeapon weapon1("Basic Sword", 3);
+//std::cout<<weapon1.GetName()<< " damage: "<< weapon1.GetDamage()<<std::endl;
 
-MagicWeapon weapon2("Magic Sword", 3, 2);
-std::cout<<weapon2.GetName()<< " damage: "<< weapon2.GetDamage()<<std::endl;
+//MagicWeapon weapon2("Magic Sword", 3, 2);
+//std::cout<<weapon2.GetName()<< " damage: "<< weapon2.GetDamage()<<std::endl;
+
+MagicDecorator magic_weapon1(&weapon1, 2);
+std::cout<<magic_weapon1.GetName()<< " damage: "<< weapon1.GetDamage()+magic_weapon1.GetMagicMod()<<std::endl;
 
 
 }
