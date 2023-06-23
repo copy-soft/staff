f30094c9519be0fe63cff384663678542ef2fe2a
Denis Vergun <blu.eagle@mail.ru>
2023-06-23 19:10:52 +0500

    cd Intial;make  //18

---
+++ Intial/ICharacter.h
@@ -0,0 +1,9 @@
+class ICharacter {
+	public:
+	int getStrength(){return 18;};
+	int getDexterity();
+	int getConstitution();
+	int getIntelligence();
+	int getWisdom();
+	int getCharisma();
+};
---
+++ Intial/main.cpp
@@ -0,0 +1,8 @@
+#include "IRanger.h"
+#include <iostream>
+using namespace std;
+int main() {
+	IRanger* drizztDoUrden = new IRanger();
+	cout<<drizztDoUrden->getStrength()<<endl;;
+return 0;
+}
