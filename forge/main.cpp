2d19eb0b98845cd48b0c1a0ee6446643d1e9bc2a
Denis Vergun <blu.eagle@mail.ru>
2023-06-30 06:31:21 +0500

    cd forge;make

--- forge/main.cpp
+++ forge/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "item.h"
 int main(){
-ConcreteWeapon weapon1("базовый меч", 3);
-std::cout<<weapon1.GetName()<< " повреждение: "<< weapon1.GetDamage()<<std::endl;
+ConcreteArmor weapon1("базовый меч", 3);
+std::cout<<weapon1.GetName()<< " повреждение: "<< weapon1.GetDamageRes()<<std::endl;
 
 //ConcreteArmor()
 
@@ -14,7 +14,7 @@ Magic magic_weapon1(&weapon1, 2);
 //Weapon weap1("test",1);
 
 magic_weapon1.SetName("Армагеддон");
-std::cout<<magic_weapon1.GetName()<< " повреждение: "<< weapon1.GetDamage()+magic_weapon1.GetMagicMod()<<std::endl;
+std::cout<<magic_weapon1.GetName()<< " повреждение: "<< weapon1.GetDamageRes()+magic_weapon1.GetMagicMod()<<std::endl;
 //std::cout<<x2_magic_weapon1.GetName()<< " повреждение: "<< weapon1.GetDamage()+magic_weapon1.GetMagicMod()<<std::endl;
 
 
