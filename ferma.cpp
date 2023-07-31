2f139747c38a8e56cd0700ccefd1976ae69a559f
Denis Vergun <blu.eagle@mail.ru>
2023-07-31 14:14:36 +0500

    make

--- ferma.cpp
+++ ferma.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-int main(){
-	int p=7;
-	int a=5;
-	for(int i=0;i<p;i++)
-	std::cout<< a*i%p <<std::endl;
+class Ferma 
+{
+    	public:
+	int operator=(Ferma fm);
+	int p;
+};
 
+// реализация операторной функции operator=()
+int Ferma::operator=(Ferma fm)
+{
+	return p%(fm.p);
+}
+
+int main(){
+	Ferma prime1{7};
+	Ferma prime2{5};
+	for(int i=0;i<prime2.p;i++){
+	int j=prime1=prime2;
+	std::cout<< j << std::endl;
+	}
 return 0;
 }
