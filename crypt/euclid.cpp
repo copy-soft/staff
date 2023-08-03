9a824101ffe62fc47ff9bea04b627574b036cc47
Denis Vergun <blu.eagle@mail.ru>
2023-08-03 16:44:39 +0500

    cd crypt;make

---
+++ crypt/euclid.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+//binary search
+
+int gcd(int a, int b){
+
+if (b==0) return a;
+std::cout<<"a="<<a<<" b="<<b<<std::endl;
+if((a-b)<(b/2))
+	return gcd(b,a-b);
+
+}
+
+
+int main(){
+
+	std::cout<<gcd(33,27)<<std::endl;
+
+
+
+return 0;
+}
---
+++ crypt/ferma.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+class Ferma 
+{
+    	public:
+	int operator=(Ferma fm);
+	int p;
+	int i;
+};
+
+int Ferma::operator=(Ferma fm)
+{
+	return i*p%(fm.p);
+}
+int main(){
+	Ferma prime1{5};
+	Ferma prime2{7};
+	for(prime1.i=0;prime1.i<prime2.p;prime1.i++)std::cout<<((int)(prime1=prime2))<<std::endl;
+return 0;
+}
