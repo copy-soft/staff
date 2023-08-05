398ce188efd2a7821066c822a651005751f54d44
Denis Vergun <blu.eagle@mail.ru>
2023-08-05 23:36:53 +0500

    ecu.cpp

---
+++ ecu.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+int main(){
+
+//for(double i =-1;i<1;i=i+0.2)
+double i = -0.5;
+	std::cout<<"x="<<i<<std::endl;//" y^2 ="<<
+	double j=(sqrt(pow(i,3)-i));
+			//<<std::endl;
+
+	std::cout<<j<<std::endl;
+
+
+
+
+return 0;
+}
