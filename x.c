7cbfae6f1939e02674ee83b187d4aa4d5a383148
Denis Vergun <blu.eagle@mail.ru>
2023-08-13 15:36:46 +0500

    make

--- x.c
+++ x.c
@@ -37,10 +37,10 @@ char *text[255];
 		//printf("You presed %c key\n", text[0]);}
 	//if(event.type==ButtonPress){
 		XSetForeground(dis, gc, white);
-		XDrawLine(dis, win, gc, 150, 300, 150, 0);
-                XDrawLine(dis, win, gc, 0, 150, 300, 150);
+		XDrawLine(dis, win, gc, 500, 1000, 500, 0);
+                XDrawLine(dis, win, gc, 0, 500, 1000, 500);
                 for(int i=10;i<120;i+=10)
-		XDrawLine(dis, win, gc, 90+i, 145, 90+i, 155);
+		//XDrawLine(dis, win, gc, 90+i, 145, 90+i, 155);
 		//XDrawPoint(dis, win ,gc, event.xbutton.x, event.xbutton.y);
                 std::cout<<"elipptic curve sqr(x(x+1)(2x+1))"<<std::endl;
 		for(double x =-5;x<10;x+=0.01){
@@ -50,8 +50,8 @@ char *text[255];
                 //XDrawPoint(dis, win ,gc, (i*20)+150, 150-(sqrt(pow(i,3)-i))*20);
 //XDrawPoint(dis, win ,gc, (i+150), fmod((sqrt(pow(i,3)-7*i+10)),61)+150);
 //XDrawPoint(dis, win ,gc, (i)+150, 150-fmod((sqrt(pow(i,3)-7*i+10)),61));
-XDrawPoint(dis, win ,gc, (x*100+150), ((sqrt(x*(x+1)*(2*x+1)/6)*100))+150);
-XDrawPoint(dis, win ,gc, (x*100+150), 150-((sqrt(x*(x+1)*(2*x+1)/6)*100)));
+XDrawPoint(dis, win ,gc, (x*100+500), ((sqrt(x*(x+1)*(2*x+1)/6)*100))+500);
+XDrawPoint(dis, win ,gc, (x*100+500), 500-((sqrt(x*(x+1)*(2*x+1)/6)*100)));
 //XDrawPoint(dis, win ,gc, (i*7)+150, 150-((sqrt(pow(i,3)-7*i+10)*7)));
 //if(i==-3)XDrawPoint(dis, win ,gc, (i*7+149), 150-((sqrt(pow(i,3)-7*i+10)*7)));
 //if(i==-1)XDrawPoint(dis, win ,gc, (i*7+149), 150-((sqrt(pow(i,3)-7*i+10)*7)));
@@ -60,11 +60,11 @@ XDrawPoint(dis, win ,gc, (x*100+150), 150-((sqrt(x*(x+1)*(2*x+1)/6)*100)));
 //XDrawLine(dis, win, gc, j*7+150, 150-((sqrt(pow(j,3)-7*j+10)*7)), k*7+150, 150-((sqrt(pow(k,3)-7*k+10)*7)));
 //std::cout<<((sqrt(pow(k,3)-7*k+10)))<<std::endl;
 //std::cout<<(-1-3)/(((sqrt(pow(k,3)-7*k+10))))-(((sqrt(pow(j,3)-7*j+10))))<<std::endl;
-XDrawPoint(dis, win ,gc, (x*100+150), 150-((sqrt(pow(x,3)-(pow(x,2)*(3/2))+(x*1/2))*100)));
-XDrawPoint(dis, win ,gc, (x*100+150), ((sqrt(pow(x,3)-(pow(x,2)*(3/2))+(x*1/2))*100))+150);
+XDrawPoint(dis, win ,gc, (x*100+500), 500-((sqrt(pow(x,3)-(pow(x,2)*(3/2))+(x*1/2))*100)));
+XDrawPoint(dis, win ,gc, (x*100+500), ((sqrt(pow(x,3)-(pow(x,2)*(3/2))+(x*1/2))*100))+500);
 
 
-XDrawPoint(dis, win ,gc, (x*100+150), (sqrt(pow((x*3-2),2)*100))+150);
+XDrawPoint(dis, win ,gc, (x*100+500), (sqrt(pow((x*3-2),2)*100))+500);
 
 //XDrawPoint(dis, win ,gc, (i*7)+150, (122-3*(i-(-1))));
 //std::cout<<k<<150-((sqrt(pow(k,3)-7*k+10)*7))<<std::endl;
@@ -88,7 +88,7 @@ dis=XOpenDisplay(NULL);
 screen=DefaultScreen(dis);
 black=BlackPixel(dis,screen);
 white=WhitePixel(dis,screen);
-win=XCreateSimpleWindow(dis, DefaultRootWindow(dis),0,0,300,300,5,white,black);
+win=XCreateSimpleWindow(dis, DefaultRootWindow(dis),0,0,1000,1000,5,white,black);
 XSetStandardProperties(dis, win, "Hi", "hi", None, NULL, 0, NULL);
 XSelectInput(dis, win, ExposureMask | ButtonPressMask | KeyPressMask);
 gc=XCreateGC(dis, win, 0, 0);
