9361281e99a941728c6e1b478d4faaf1fc62f56c
Denis Vergun <blu.eagle@mail.ru>
2023-08-25 13:29:11 +0500

    make;//gmp.cpp

--- gmp.cpp
+++ gmp.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <gmpxx.h>
 
 using namespace std;
@@ -10,10 +11,11 @@ int main (void) {
     a = 12347777.7799995555;
     b = -56788888.5558888882;
     c = a+b;
-
-    cout << "sum of " << a << " and " << b << " is " << c << "\n";
+    cout << setprecision(24);
+    cout << "sum of " << a.get_d() << " and " << b << " is " << c << "\n";
     cout << "absolute value is " << abs(c) << "\n";
     // cin >> a;
+    //cout <<
     cout <<6629162717921697/536870912<<endl;
     return 0;
 }
