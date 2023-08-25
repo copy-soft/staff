2f7a56cd4319eed22737c2ff01d016294adf7441
Denis Vergun <blu.eagle@mail.ru>
2023-08-25 17:15:47 +0500

    make

--- gmp.cpp
+++ gmp.cpp
@@ -6,16 +6,21 @@ using namespace std;
 
 int main (void) {
     
-	mpq_class a, b, c;
+	mpf_class a,c ;
+	unsigned long int b;
+	//mpf_t c;
+	//mpf_set_default_prec(100000);
+    //mpf_init(c);
+    a = 12.3;
+    b = 12;
+    mpf_pow_ui(c.get_mpf_t(), a.get_mpf_t(), b);
+    gmp_printf("c = %.50Ff\n", c);
 
-    a = 12347777.7799995555;
-    b = -56788888.5558888882;
-    c = a+b;
-    cout << setprecision(24);
-    cout << "sum of " << a.get_d() << " and " << b << " is " << c << "\n";
-    cout << "absolute value is " << abs(c) << "\n";
+    //cout << setprecision(40);
+    //cout << "sum of " << a.get_d() << " and " << b << " is " << c.get_d() << "\n";
+    //cout << "absolute value is " << abs(c) << "\n";
     // cin >> a;
     //cout <<
-    cout <<6629162717921697/536870912<<endl;
+    //cout <<6629162717921697/536870912<<endl;
     return 0;
 }
