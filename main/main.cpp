93c931af879ac2975dd33b720861a027fa6e52cc
Denis Vergun <blu.eagle@mail.ru>
2023-12-21 00:49:54 +0500

    cd main;make

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <string>
 int main (int argc, const char * argv[]) { 
-if(argc>1)std::cout<< argv[1] <<std::endl;
+if(argc>1){
+if((argv[1])=1)
+{
+std::cout<< "int" <<std::endl;
+}}
 return 0;
 }
