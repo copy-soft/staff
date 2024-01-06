c2e83d30e7201476d81d9105e0053b767ce83095
Denis Vergun <blu.eagle@mail.ru>
2024-01-07 01:51:49 +0500

    sqare;make

---
+++ square/1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class Square{
+	public:
+	Square(){}
+	int S;
+	int P;
+	int calc_s(int a){S=a*a;return S;}
+
+}i;
+int main(){
+Square A;
+A.calc_s(3);
+std::cout<<A.S<<std::endl;
+
+return 0;
+}
