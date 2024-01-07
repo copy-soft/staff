187e7b20144a58451e3bbbbb9c2c922a9cdfca00
Denis Vergun <blu.eagle@mail.ru>
2024-01-08 04:13:36 +0500

    квадрат

--- square/1.cpp
+++ square/1.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
-
-class Square{
+class Square{//обьект квадрат
 	public:
 	Square(){}
-	int S;
-	int P;
-	int calc_s(int a){S=a*a;return S;}
-	int calc_p(int a){P=4*a;return P;}
-	bool operator == (Square a){
+	int S;//площадь квадрата
+	int P;//периметр квадрата
+	int calc_s(int a){S=a*a;return S;}//вычисление площади квадрата
+	int calc_p(int a){P=4*a;return P;}//вычисление периметра квадрата
+	bool operator == (Square a){//
 	if(this->S==a.S)return true;
-	if(this->S!=a.S)return false;
+	else return false;
 	}
 
 }i;
 int main(){
 Square A, B;
 A.calc_s(3);
-B.calc_s(2);
-if(A==B)std::cout<<A.S<<std::endl;
-
+B.calc_s(3);
+if(A==B)std::cout<<"квадраты равны"<<std::endl;
+else std::cout<<"квадраты не равны"<<std::endl;
 return 0;
 }
