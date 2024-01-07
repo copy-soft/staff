46e287372aac9a9cd169a685c6f13d1a750233d0
Denis Vergun <blu.eagle@mail.ru>
2024-01-07 05:54:12 +0500

    cd square;make

--- square/1.cpp
+++ square/1.cpp
@@ -8,13 +8,15 @@ class Square{
 	int calc_s(int a){S=a*a;return S;}
 	int calc_p(int a){P=4*a;return P;}
 	bool operator == (Square a){
-	return true;
+	if(this->S==a.S)return true;
+	if(this->S!=a.S)return false;
 	}
 
 }i;
 int main(){
 Square A, B;
 A.calc_s(3);
+B.calc_s(2);
 if(A==B)std::cout<<A.S<<std::endl;
 
 return 0;
