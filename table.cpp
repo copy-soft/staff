23c4862065be243c533fac6292622a6ccd4c6708
Denis Vergun <blueagle.so.0@gmail.com>
2024-05-25 01:18:12 +0500

    g++ table.cpp;./a.out

--- table.cpp
+++ table.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <iomanip>
 int main(){
-int a[5]={0};
+int a[5]={0}, *p;
+p=&a[0];
 std::cout<<"элемент"<<std::setw(13)<<"\tзначение"<<std::endl;
 for (int i=0;i<5;i++)
-std::cout<<std::setw(4)<<i<< std::setw(8)<<a[i]<<std::endl;
+std::cout<<std::setw(4)<<p+i<< std::setw(8)<<a[i]<<std::endl;
 
 
 
