34fb966f82c094af1dfc9a40c8b64ba0b5d5d8a5
Денис Вергун <blueagle@gnail.com>
2024-09-23 12:09:06 +0500

    x ,  make

--- x/lambda.cpp
+++ x/lambda.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+namespace foo{
+	int number(){
+	return 1;
+	}
+}
+namespace bar{
+	int number(){
+	return 3;
+	}
+}
+
+
+
+
 auto main(int argc, char *argv[])->int {
 
-std::cout<<argv[1]<<std::endl;
+int result=foo::number()+bar::number();
+	
+std::cout<<result<<std::endl;
+
+
 return -(-![]{});
 
 }
