2475b7d03bf16aa1e40dcf4e0ecd75ef09045b22
Денис Вергун <blueagle@gnail.com>
2025-01-25 18:09:06 +0500

    make;./a.out

--- main5.cpp
+++ main5.cpp
@@ -9,9 +9,19 @@ main(int argc, char *argv[])
 {
     (void) initscr();      /* initialize the curses library */
 	
+	curs_set(0);
 	for (int i=0;i<20;i++){
         clear();
-	mvprintw(i, 30, ">rocket>");
+	start_color();
+	init_pair(1, COLOR_BLACK, COLOR_BLUE);
+	init_pair(2, COLOR_BLACK, COLOR_RED);
+	init_pair(3, COLOR_BLACK, COLOR_WHITE);
+        attron(COLOR_PAIR(3));
+	mvprintw(i, 30, ">ro");
+        attron(COLOR_PAIR(1));
+	mvprintw(i, 33,	"ck");
+        attron(COLOR_PAIR(2));
+	mvprintw(i, 35,	"et>");
 	sleep(1);
 	//clear();
 	refresh();
