3f672af648df47834d3ee735b153629cba29189a
Денис Вергун <blueagle@gnail.com>
2025-01-27 18:49:24 +0500

    make

--- main9.cpp
+++ main9.cpp
@@ -15,26 +15,44 @@ main(int argc, char **argv) {
     Window    root;
     Screen   *screen;
     GC        gc;
-    int  h, w;
+    Pixmap    pm;
+    int depth,  h, w;
     char *img;
     dpy = XOpenDisplay(0);
     screen = DefaultScreenOfDisplay(dpy);
     h = HeightOfScreen(screen);
     w = WidthOfScreen(screen);
     root = DefaultRootWindow(dpy);
-    //depth = DefaultDepth(dpy, DefaultScreen(dpy));
+    depth = DefaultDepth(dpy, DefaultScreen(dpy));
     gc = XCreateGC(dpy, pm 	, 0, NULL);
-    XSetForeground (dpy, gc, _RGB(255, 255, 0));
+    pm = XCreatePixmap(dpy, root, w, h, depth);
+    gc = XCreateGC(dpy, pm, 0, NULL);
+
+	XSetForeground (dpy, gc, _RGB(255, 255, 0));
+	//штабель
 	int j=0;
 	for(int i=0;i<=20;i++){
-	if(i>5)j=i-5;
+	if(i>8)j=i-8;
 	XDrawLine(dpy, root, gc, 100, 100+i, 200-j, 100+i);
 	
 
 
 	}
     
-    XMapWindow(dpy, root);
+        XSetForeground (dpy, gc, _RGB(0, 0, 0));
+
+
+
+    XDrawLine(dpy, root, gc, 100, 100, 200, 100);
+    XDrawLine(dpy, root, gc, 100, 120, 189, 120);
+    XDrawLine(dpy, root, gc, 100, 120, 100, 100);
+    XDrawLine(dpy, root, gc, 200, 100, 200, 109);
+
+
+
+
+
+     XMapWindow(dpy, root);
 
     XFlush(dpy);
     XCloseDisplay(dpy);
