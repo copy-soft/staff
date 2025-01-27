34779493cb5dcab8097a67d00644b68590798b84
Денис Вергун <blueagle@gnail.com>
2025-01-27 18:34:41 +0500

    make

--- main9.cpp
+++ main9.cpp
@@ -3,51 +3,40 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <cstdio>
+
+unsigned long _RGB(int r,int g, int b)
+{
+    return b + (g<<8) + (r<<16);
+}
+
 int
 main(int argc, char **argv) {
     Display  *dpy;
     Window    root;
     Screen   *screen;
     GC        gc;
-    XImage   *ximg;
-    Pixmap    pm;
-    int depth, h, w;
+    int  h, w;
     char *img;
     dpy = XOpenDisplay(0);
-    if (!dpy) {
-        fputs("cannot open display\n", stderr);
-        return 1;
-    }
     screen = DefaultScreenOfDisplay(dpy);
     h = HeightOfScreen(screen);
     w = WidthOfScreen(screen);
     root = DefaultRootWindow(dpy);
-    depth = DefaultDepth(dpy, DefaultScreen(dpy));
-    img = malloc(depth/8 * h * w);
-    if (img == NULL) {
-        perror("malloc() failed");
-        return 1;
-    }
-    memset(img, 0xFF, depth/8 * h * w);
-    ximg = XCreateImage(dpy, CopyFromParent, depth, ZPixmap, 0, img, w, h, 32, 0);
-    pm = XCreatePixmap(dpy, root, w, h, depth);
-    gc = XCreateGC(dpy, pm, 0, NULL);
-    XDrawLine(dpy, root, gc, 100, 100, 200, 100);
-    XDrawLine(dpy, root, gc, 100, 120, 190, 120);
-    XDrawLine(dpy, root, gc, 100, 120, 100, 100);
-    XDrawLine(dpy, root, gc, 200, 100, 200, 110);
+    //depth = DefaultDepth(dpy, DefaultScreen(dpy));
+    gc = XCreateGC(dpy, pm 	, 0, NULL);
+    XSetForeground (dpy, gc, _RGB(255, 255, 0));
+	int j=0;
+	for(int i=0;i<=20;i++){
+	if(i>5)j=i-5;
+	XDrawLine(dpy, root, gc, 100, 100+i, 200-j, 100+i);
+	
 
+
+	}
+    
     XMapWindow(dpy, root);
 
     XFlush(dpy);
-    //XDrawArc(dpy, root, gc, 50-(30/2), 100-(30/2), 30, 30, 0, 360*64);
-    //XFlush(dpy);
-    //XPutImage(dpy, pm, gc, ximg, 0, 0, 0, 0, w, h);
-    //XCopyArea(dpy, pm, root, gc, 0, 0, w, h, 0, 0);
-    //free(img);
-    //XFreePixmap(dpy, pm);
-    //XDestroyImage(ximg);
-    //XFreeGC(dpy, gc);
     XCloseDisplay(dpy);
     return 0;
 }
