2b310c2bcc199b08dca8310e2298304ac1154dbe
Денис Вергун <blueagle@gnail.com>
2025-02-17 07:14:10 +0000

    CFLAGS="-g -O0" make asm;./asm

--- asm.c
+++ asm.c
@@ -1,13 +1,13 @@
 int main(void){
 
-asm("push   %rbp");
-asm("mov %rsp,%rbp");
-asm("movl $0x5,-0x8(%rbp)");
-asm("mov -0x8(%rbp),%eax");
-asm("add $0x6,%eax");
-asm("mov %eax,-0x4(%rbp)");
-asm("mov $0x0,%eax");
-asm("pop %rbp");
+asm("push   %rbp\n"
+"mov %rsp,%rbp\n"
+"movl $0x5,-0x8(%rbp)\n"
+"mov -0x8(%rbp),%eax\n"
+"add $0x6,%eax\n"
+"mov %eax,-0x4(%rbp)\n"
+"mov $0x0,%eax\n"
+"pop %rbp");
 //asm("ret");
 
 }
